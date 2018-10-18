c0f0f01252bf791a09241d9a2770915df48a9483
Marco Piccinini <marcpiccio@gmail.com>
2018-10-18 12:17:19 +0200

    try delete function (commen

--- src/App.js
+++ src/App.js
@@ -11,17 +11,26 @@ class App extends Component {
 		todos: []
 	};
 	this.onChange = this.onChange.bind(this);
-	this.onSubmitText = this.onSubmitText.bind(this);
+  this.onSubmitText = this.onSubmitText.bind(this);
+  this.onClick=this.onClick.bind(this);
   }
   onChange(event) {
 	this.setState({ currentTodo: event.target.value });
   }
+
   onSubmitText(event) {
     event.preventDefault();
     this.setState({
 	  currentTodo: '',
       todos: [...this.state.todos, this.state.currentTodo],
     });
+  }
+
+  onClick(event, index){
+    event.preventDefault();
+    this.setState({
+      todos:[index]=""
+    })
   }
   
   render() {
@@ -34,7 +43,7 @@ class App extends Component {
 		<div className="lista">
 			<ol>
 			{
-			  this.state.todos.map((todo, index) => <li key={index}>{todo}</li>)
+			  this.state.todos.map((todo, index) => <li /*onClick={(event, index) => this.delete}*/ key={index}>{todo}</li>)
 			}
 			</ol>
 		</div>
